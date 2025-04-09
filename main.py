e530c8f40f3c06f2814b88abd0bb31d9c1016272
Haritiana Adriano <hei.adriano.4@gmail.com>
2025-04-09 14:08:55 +0300

    feat: fetch related movies

--- main.py
+++ main.py
@@ -4,6 +4,29 @@ from fastapi.responses import JSONResponse
 
 app = FastAPI()  
 
+@app.get("/related-movies")
+def get_related_movies(id: str):
+    url = "https://imdb232.p.rapidapi.com/api/title/get-more-like-this"
+
+    querystring = {"tt": id}
+
+    headers = {
+        "x-rapidapi-key": "71b2e05dbamsh0a26810352c0cdep1c8a58jsn19bd02ce5cdc",
+        "x-rapidapi-host": "imdb232.p.rapidapi.com"
+    }
+    
+    response = requests.get(url, headers=headers, params=querystring)
+    
+    # Check if the external API call was successful
+    if response.status_code == 200:
+        # Return the JSON data from the external API
+        return JSONResponse(content=response.json())
+    else:
+        # Return an error message if the external API fails
+        return JSONResponse(content={"error": "Failed to fetch data"}, status_code=response.status_code)
+
+
+
 @app.get("/movies")
 def get_movies():
     url = "https://imdb232.p.rapidapi.com/api/title/get-most-popular"
